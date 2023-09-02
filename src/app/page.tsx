9fbb21189ba28033a09ff9e2b4094cc3ae2e081e
kiratopat <capacity1412@gmail.com>
2023-09-02 19:24:47 +0700

    V1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,171 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import Inputs from "@/components/Inputs";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  function VariableInput({
+    var_name,
+    var_type,
+    var_value,
+    var_unit,
+    disabled,
+    onChange,
+  }: {
+    var_name: string;
+    var_type: string;
+    var_value: string;
+    var_unit: string;
+    disabled?: boolean;
+    onChange?: (e: any) => void;
+  }) {
+    function checkVarName(varname: string) {
+      if (varname === "I") {
+        return (
+          <>
+            I<sub>c</sub>
+          </>
+        );
+      } else if (varname === "V") {
+        return (
+          <>
+            V<sub>Ø</sub>
+          </>
+        );
+      } else if (var_name === "B") {
+        return (
+          <>
+            B<sub>c</sub>
+          </>
+        );
+      } else {
+        return varname;
+      }
+    }
+
+    return (
+      <>
+        <div className="relative mb-4 flex flex-wrap items-stretch">
+          <span className=" font-bold flex items-center whitespace-nowrap rounded-l border border-r-0 border-solid border-neutral-300 px-3 py-[0.25rem] text-center text-base leading-[1.6] text-neutral-700 ">
+            {checkVarName(var_name)}
+          </span>
+          <input
+            type={var_type}
+            className={`disabled:bg-gray-200 text-center relative m-0 block w-[1px] min-w-0 flex-auto border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none`}
+            aria-label="Volatage"
+            value={var_value}
+            onChange={(e) => {
+              setDistance(parseFloat(e.target.value));
+            }}
+            disabled={disabled}
+            pattern="[0-9]+([,][0-9]{1,2})?"
+          />
+          <span className="flex font-bold items-center whitespace-nowrap rounded-r border border-l-0 border-solid border-neutral-300 px-3 py-[0.25rem] text-center text-base leading-[1.6] text-neutral-700">
+            {var_unit}
+          </span>
         </div>
-      </div>
+      </>
+    );
+  }
+  function SusceptanceSelector({ index }: { index: number }) {
+    return (
+      <button
+        key={index}
+        onClick={() => {
+          setBc(Susceptance[index]);
+          setCurBc(index);
+        }}
+        className={`px-4 font-bold py-3 ${
+          curBc === index
+            ? `bg-blue-500 text-white shadow-md`
+            : `bg-white text-black shadow-lg`
+        }  rounded-lg border-2 border-blue-500 transition duration-300 ease-in-out`}
+      >
+        {index + 1}
+      </button>
+    );
+  }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  const [L, setL] = useState<number>(0);
+  const [Ic, setIc] = useState<number>(0);
+  const [V, setV] = useState<number>(115);
+  const [distance, setDistance] = useState<number>(1);
+  const [Bc, setBc] = useState<number>(0);
+  const [curBc, setCurBc] = useState<number>(0);
+  // const baseIc = (V * 1000) / Math.sqrt(3);
+  const Susceptance = [3.2163, 3.123, 4.3156, 4.1464, 3.2238, 4.3326];
+  // console.log(Susceptance);
+  // console.log(Math.pow(10, 3), Math.pow(10, -6));
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+  useEffect(() => {
+    const ic = (V * Bc * Math.pow(10, -3)) / Math.sqrt(3);
+    setIc(ic);
+    const l = ((V / Math.sqrt(3)) * ic) / 2;
+    setL(l);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [Bc, distance]);
+
+  return (
+    <main className="bg-white w-screen h-auto flex justify-center my-10">
+      <div className="flex-col justify-start gap-6 rounded-lg shadow-lg w-full mx-6">
+        <h1 className="text-center text-4xl text-blue-500 font-bold drop-shadow-md">
+          Charging current calculator
+        </h1>
+        <section
+          id="params"
+          className="flex flex-col mt-4 justify-start px-6 gap-2 text-center"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <VariableInput
+            var_name="Arc lenght"
+            var_type="text"
+            var_value={(L * distance).toFixed(2).toLocaleString()}
+            var_unit="cm"
+            disabled
+          />
+          <VariableInput
+            var_name="I"
+            var_type="text"
+            var_value={(Ic * distance).toFixed(5).toLocaleString()}
+            var_unit="A"
+            disabled
+          />
+          <VariableInput
+            var_name="V"
+            var_type="text"
+            var_value={V.toLocaleString()}
+            var_unit="kV"
+            disabled
+          />
+          <VariableInput
+            var_name="Distance"
+            var_type="number"
+            var_value={distance.toLocaleString()}
+            var_unit="km"
+          />
+          <VariableInput
+            var_name="B"
+            var_type="text"
+            var_value={(Bc * distance).toFixed(5).toLocaleString()}
+            var_unit="μ℧"
+            disabled
+          />
+          <div className="flex justify-center gap-3">
+            {Susceptance.map((_, index) => {
+              return <SusceptanceSelector key={index} index={index} />;
+            })}
+          </div>
+        </section>
+        <section id="standard">
+          <Image
+            src="/PEA_structure.jpg"
+            alt="Structure standard"
+            className="w-11/1 mt-6 justify-items-center"
+            width={2000}
+            height={1}
+            priority
+          />
+        </section>
       </div>
     </main>
-  )
+  );
 }
